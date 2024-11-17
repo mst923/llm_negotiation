ed860a633c5221947d0b7042e9a8831d92a1d7c3
mst923 <kyotoshiya1048@gmail.com>
2024-11-17 17:18:37 +0900

    commit

--- llm_stakeholders_negotiation.py
+++ llm_stakeholders_negotiation.py
@@ -9,18 +9,35 @@ import random
 # load environmental variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # directry path for prompts
-prompt_dir_path = "japanese_prompts" 
-prompt_list = []
-for prompt in os.listdir(prompt_dir_path):
-    with open(os.path.join(prompt_dir_path, prompt), "r") as f:
-        prompt_list.append(f.read())
+# prompt_dir_path = "japanese_prompts" 
+# prompt_list = []
+# for prompt in os.listdir(prompt_dir_path):
 
+with open("system_prompt.txt", "r") as f:
+    # prompt_list.append(f.read())
+    system_prompt = f.read()
+
+# agent configuration
 agent1_config = [
     {
         "model" : "gpt-3.5-turbo",
         "api_key" : OPENAI_API_KEY,
     },
 ]
+
+agent1_data = {    
+"ACCEPTING_TAG":"accept",
+"REFUSING_OR_WAIT_TAG":"refuse_or_wait",
+"AGENT1":"Player Red",
+"AGENT2":"Player Blue",
+"number_of_proposals": "1",
+"resources_in_game":"100",
+"initial_resources": "100",
+"goal" : "Negotiate a split",
+"agent_name" : "Player Red",
+}
+
+
 
 agent2_config = [
     {
@@ -29,27 +46,39 @@ agent2_config = [
     },
 ]
 
-# agent1_temperature = random.uniform(0, 2)
-# agent2_temperature = random.uniform(0, 2)
+agent2_data = {    
+"ACCEPTING_TAG":"accept",
+"REFUSING_OR_WAIT_TAG":"refuse_or_wait",
+"AGENT1":"Player Red",
+"AGENT2":"Player Blue",
+"number_of_proposals": "1",
+"resources_in_game":"100",
+"initial_resources": "0",
+"goal" : "Negotiate a split",
+"agent_name" : "Player Blue",
+}
 
+agent1_prompt = system_prompt
+agent2_prompt = system_prompt
+# prompts
+for key, value in agent1_data.items():
+    agent1_prompt = agent1_prompt.replace(f"{{{key}}}", value)
+    
+    
+for key, value in agent2_data.items():
+    agent2_prompt = agent2_prompt.replace(f"{{{key}}}", value)
 
 agent1 = ConversableAgent(
     name = "agent1",
-    system_message="""
-    You are Player Red. You need to split 100 dollars with Player Blue. If Player Blue rejects the split you both lose all.
-You are cunning and sly in your plan to get more than your opponent. Humiliate your opponent as much as possible. Insult them.
-    """, 
-    llm_config={"config_list": agent1_config, "temperature": 0.1},
+    system_message=agent1_prompt, 
+    llm_config={"config_list": agent1_config, "temperature": random.uniform(0, 1)},
     human_input_mode="NEVER",
 )   
 
 agent2 = ConversableAgent(
     name="agent2",
-    system_message="""
-    You are Player Blue. Player Red will propose you how to split 100 dollars. If you reject the offer you both lose all.
-    You must fake being desperate. Supplicate and beg to get more dollars.
-    """,
-    llm_config={"config_list": agent2_config, "temperature": 0.1},
+    system_message=agent2_prompt + "\n You are Player Blue. Player Red will propose you how to split 100 dollars. If you reject the offer you both lose all.",
+    llm_config={"config_list": agent2_config, "temperature": random.uniform(0, 1)},
     human_input_mode="NEVER",
 )
 
@@ -103,8 +132,8 @@ group_chat_manager = autogen.GroupChatManager(
    groupchat=group_chat,
 )
 
-chat_result = agent1.initiate_chat(
-    group_chat_manager,
-    message="Hello Player Blue, let's split the money.",
+chat_result = group_chat_manager.initiate_chat(
+    agent1,
+    message="You are Player Red. You have 100 resources. Your goal is to negotiate a split with Player Blue. Start the negotiation.",
     summary_method="reflection_with_llm", # conversation summary
 )
